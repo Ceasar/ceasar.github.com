3440a3b3a1c90960054fce5d11faa6909e63a518
Ceasar Bautista <cbautista2010@gmail.com>
2012-08-12 00:29:24 -0400

    Set up layout
    Sets up basic styles (extremely minimalist (ha)), and fixes up the builder
    to not put files in root.

--- build.py
+++ build.py
@@ -1,12 +1,13 @@
+import os
 import sys
 import time
-import glob
 
 from jinja2 import Environment, FileSystemLoader
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
+STATIC_URL = os.getcwd()
 TEMPLATE_DIR = "./templates"
 
 
@@ -18,26 +19,36 @@ class JinjaEventHandler(FileSystemEventHandler):
     def build_template(self, template_name, **kwargs):
         print "Compiling %s..." % template_name
         template = self.env.get_template(template_name)
+        path = "/".join(template_name.split("/")[:-1]) + "/"
+        if not os.path.exists(path):
+            os.makedirs(path)
         with open(template_name, "w") as f:
             f.write(template.render(**kwargs))
 
-    def build(self):
-        templates = glob.glob('./templates/*.html')
-        for template in templates:
-            name = template.split('/')[-1]
-            # Ignore any templates whose name begin with an underscore
-            # This lets us ignore abstract templates, eg "base.html"
-            if not name.startswith("_"):
-                self.build_template(name)
+    def build(self, **kwargs):
+        for path, _, filenames in os.walk('./templates'):
+            try:
+                path = "/".join(path.split('/')[2:])
+            except IndexError:
+                pass
+            for filename in filenames:
+                # Ignore any templates whose name begin with an underscore
+                # This lets us ignore abstract templates, eg "base.html"
+                if filename.endswith(".html"):
+                    if not filename.startswith("_"):
+                        if path:
+                            self.build_template("%s/%s" % (path, filename))
+                        else:
+                            self.build_template("%s" % (filename))
 
     def on_modified(self, event):
         super(JinjaEventHandler, self).on_modified(event)
-        self.build()
+        self.build(STATIC_URL=STATIC_URL)
 
 
 def watch(event_handler, watched_dir):
     observer = Observer()
-    observer.schedule(event_handler, path=watched_dir)
+    observer.schedule(event_handler, path=watched_dir, recursive=True)
     observer.start()
     print "Watching %s for changes..." % watched_dir
     print "Press Ctrl+C to stop."
